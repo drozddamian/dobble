247ab57046837691a6b44ec1f76444f1234dff25
Damian Drozd <damiandrozd.dev@gmail.com>
2020-09-01 14:33:38 +0200

    + Add Redux for Notifications

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Notification = () => (
+  <Wrapper>
+
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+
+`
+
+export default Notification
--- src/components/Page/PageTitle.tsx
+++ src/components/Page/PageTitle.tsx
@@ -32,17 +32,34 @@ const Wrapper = styled.div`
   ${Button} {
     margin-right: 8px;
   }
+  
+  @media (min-width: ${({ theme })=> theme.rwd.mobile.m}) {
+    justify-content: flex-start;
+    padding-left: 40px;
+  }
 `
 
 const Title = styled.h1`
   font-family: ${({ theme }) => theme.fonts.russo};
   font-size: ${({ theme }) => theme.fontSize.s27};
   color: ${({ theme }) => theme.colors.darkBlue};
-
   max-width: 290px;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+  
+  @media (min-width: ${({ theme })=> theme.rwd.mobile.s}) {
+    min-width: 350px;
+  }
+  
+  @media (min-width: ${({ theme })=> theme.rwd.mobile.m}) {
+    min-width: 500px;
+  }
+  
+  @media (min-width: ${({ theme })=> theme.rwd.tablet.s}) {
+    white-space: normal;
+    min-width: 90%;
+  }
 `
 
 export default PageTitle
