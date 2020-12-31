eed04e08adc335f33c3552410681f17a0d96c65b
drozddamian <damiann74@gmail.com>
2020-12-31 16:14:17 +0100

    Fix api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,6 @@
-export const API_SERVER_URL = 'http://localhost:5000'
+export const API_SERVER_URL = process.env.NODE_ENV === 'development'
+  ? 'https://dev-dobble.herokuapp.com'
+  : 'https://prod-dobble.herokuapp.com'
 
 const API = {
   AUTHENTICATION: `${API_SERVER_URL}/api/authentication`,
